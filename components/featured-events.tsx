a67c3fed01677f9f7011d337388e79970420840d
kamau-n <bsclmr146620@spu.ac.ke>
2025-05-23 13:37:12 +0300

    a  few changes

--- components/featured-events.tsx
+++ components/featured-events.tsx
@@ -82,7 +82,7 @@ export default function FeaturedEvents() {
           </Button>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-6">
           {displayEvents.map((event) => {
             const isTest = event.data_mode === "test";
 
