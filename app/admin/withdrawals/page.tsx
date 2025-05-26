db6ab79cef9c5b060029e79d6850ac6437a819c9
KamauHarrison <100772289+kamau-n@users.noreply.github.com>
2025-05-26 08:47:46 +0300

    Update page.tsx

--- app/admin/withdrawals/page.tsx
+++ app/admin/withdrawals/page.tsx
@@ -314,6 +314,7 @@ export default function AdminWithdrawalsPage() {
 					withdrawalId,
 					accountDetails: withdrawalData.accountDetails,
 					amount: withdrawalData.amount,
+					eventId:withdrawalData.eventId,
 					source, // Include the selected source
 				}),
 			});
